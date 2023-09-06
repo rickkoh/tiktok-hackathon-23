32c0d32ec36e08f84446f9c1dc6ede695e5274bf
Rick <rick.kohjiaxuan@gmail.com>
2023-09-06 12:39:59 +0800

    removed product component.

--- src/components/ComponentRegistry/ProductComponent.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductComponent() {
-  return (
-    <div className="w-full h-fit px-3 py-2 items-center rounded-lg flex flex-row bg-gray-100 gap-2 my-1">
-      <p className="m-5 bg-pink-300 py-5 px-3">image</p>
-      <p>Proin vehicula dui at odio commodo, et sollicitudin purus semper.</p>
-    </div>
-  );
-}
