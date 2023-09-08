bb9c0e44b14913436202791e19437ff051914713
Rick <rick.kohjiaxuan@gmail.com>
2023-09-08 20:35:50 +0800

    add link to create blog.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Blog, { BlogRef } from "@/components/Blog/Blog";
 import ExampleComponent from "@/components/ExampleComponent";
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 export default function Home() {
   const blogRef = useRef<BlogRef>(null);
 
   return (
-    <main className="flex flex-col w-full h-[100dvh] bg-secondary">
+    <main className="flex flex-col w-full h-[100dvh] bg-secondary gap-4">
       <h1>main page</h1>
       <ExampleComponent />
       <button
@@ -20,6 +21,12 @@ export default function Home() {
       >
         Open widget
       </button>
+      <Link
+        href="/new"
+        className="border w-fit h-fit border-black px-3 py-1 bg-blue-300"
+      >
+        New Blog
+      </Link>
       <Blog ref={blogRef} />
     </main>
   );
