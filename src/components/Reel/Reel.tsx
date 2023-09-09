ea0c212275458b8e736a3375c2b2a02df3597706
Rick <rick.kohjiaxuan@gmail.com>
2023-09-10 07:15:24 +0800

    added playsInLine attribute.

--- src/components/Reel/Reel.tsx
+++ src/components/Reel/Reel.tsx
@@ -10,7 +10,7 @@ export interface ReelProps {
 function Reel(props: ReelProps) {
   return (
     <div className={"h-[calc(100vh-72px)] w-screen relative bg-black"}>
-      <video loop autoPlay muted className="h-full">
+      <video loop autoPlay muted playsInline className="h-full">
         <source src={props.reel.videoUrl} type="video/mp4" />
       </video>
       <ReelOverlay reel={props.reel} />
